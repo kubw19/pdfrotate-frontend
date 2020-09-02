53a3f75d375f4b75c3f19dddcc47b1fec3cdbf6b
Jakub Wlodarczyk <kubw19@gmail.com>
2020-09-02 18:44:56 +0200

    change of metadata handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontPdfRotate';
+export class AppComponent implements OnInit {
+
+  constructor(private meta: Meta, private title: Title) { };
+
+  ngOnInit(): void {
+
+    this.title.setTitle("Obracanie plików PDF, za darmo, trwale, bez znaków wodnych")
+
+    this.meta.addTags([
+      { name: "description", content: "Jak obrócić plik PDF? Za pomocą tego narzędzia możesz to zrobić szybko i za darmo. Narzędzie pozwalające bezpłatnie i szybko obrócić dowolne plii PDF online." }
+    ])
+  }
 }
